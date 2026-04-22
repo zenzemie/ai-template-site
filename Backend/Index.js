4c793ffe36becb10d4da1528e60f890d2d2672d6
zenzemie <hemmingstyler3@gmail.com>
2026-04-21 23:19:44 -0500

    Initialize Express server with CORS and dotenv

---
+++ Backend/Index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
